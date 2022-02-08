7bda2db30aa90061aa17a646a65c46b41c505044
Suharsh007 <suharshmahajan07@gmail.com>
2022-02-08 22:43:58 +0530

    Structure Changed

---
+++ Deci_to_Binary.java
@@ -0,0 +1,19 @@
+
+import java.util.*;
+
+public class Deci_to_Binary {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int decimal = sc.nextInt();
+        int binary[] = new int[40];
+        int index = 0;
+        while (decimal > 0) {
+            binary[index++] = decimal % 2;
+            decimal = decimal / 2;
+        }
+        for (int i = index - 1; i >= 0; i--) {
+            System.out.print(binary[i]);
+        }
+        sc.close();
+    }
+}
